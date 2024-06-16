13bf08c00a62be29afb31e8af8021c81a8ef45e2
Takeshi Miura <101356765+Takeshi-mi@users.noreply.github.com>
2024-06-16 12:39:40 -0300

    Revert "Mudei o título na home"
    This reverts commit 7f64ba64a4f68044f228d24f3790f2f1e465b177.

--- a2/IFood/admin/includes/footer.php
+++ a2/IFood/admin/includes/footer.php
@@ -1,8 +1,8 @@
-<footer class="container-fluid footer row bg-danger text-white mt-5">
+<footer class="footer row bg-danger text-white mt-5">
     <div class="container">
         <div class="row mt-3">
             <div class="col-md-12 text-center">
-            <p>&copy; 2024 TKFood. Site feito como portifólio de <a target="blank" href="https://www.github.com/takeshi-mi">Takeshi Miura</a>.</p>
+                <p>&copy; 2024 TKFood. Todos os direitos reservados.</p>
             </div>
         </div>
     </div>
